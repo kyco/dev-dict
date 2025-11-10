77f8246921ed883cd66a5af397c965ba22ce198c
Cornelius Weidmann <caweidmann.dev@gmail.com>
2025-11-10 16:11:57 +0100

    chore: Add Node.js

--- src/term-types/index.ts
+++ src/term-types/index.ts
@@ -1,9 +1,11 @@
 import cms from './cms'
 import library from './library'
+import runtime_environment from './runtime_environment'
 
 export const TERM_TYPES = {
   [cms.id]: cms,
   [library.id]: library,
+  [runtime_environment.id]: runtime_environment,
 } as const
 
 export type TTermTypes = (typeof TERM_TYPES)[keyof typeof TERM_TYPES]
--- src/terms/index.ts
+++ src/terms/index.ts
@@ -1,8 +1,10 @@
 import type { TTerm } from '../typings'
+import node_js from './node_js'
 import react from './react'
 
 export const TERMS: Record<string, TTerm> = {
   [react.id]: react,
+  [node_js.id]: node_js,
 } as const
 
 export type TTerms = (typeof TERMS)[keyof typeof TERMS]
