e88f6814194394031043fafcb2a1960a8d0275de
Cornelius Weidmann <caweidmann.dev@gmail.com>
2025-11-26 22:07:15 +0100

    fix: Only use `examples` as root for dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  root: 'examples',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@data': resolve(__dirname, './data/index.ts'),
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+
+  return {
+    root: isDev ? 'examples' : undefined,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '@data': resolve(__dirname, './data/index.ts'),
+      },
     },
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'devdict',
-      formats: ['es', 'umd'],
-      fileName: (format) => (format === 'umd' ? 'dev-dict.min.js' : 'index.js'),
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'devdict',
+        formats: ['es', 'umd'],
+        fileName: (format) => (format === 'umd' ? 'dev-dict.min.js' : 'index.js'),
+      },
+      // rollupOptions: {
+      //   external: [],
+      //   output: {
+      //     globals: {},
+      //     exports: 'named',
+      //   },
+      // },
+      minify: true,
+      sourcemap: true,
+      outDir: 'dist',
+      emptyOutDir: true,
     },
-    // rollupOptions: {
-    //   external: [],
-    //   output: {
-    //     globals: {},
-    //     exports: 'named',
-    //   },
-    // },
-    minify: true,
-    sourcemap: true,
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
+  }
 })
