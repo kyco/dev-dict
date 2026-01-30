f31697ec105ef5910df736a13d08b124a6970760
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-01-30 11:00:54 +0100

    fix: Update wdio

--- src/data/terms/wdio.ts
+++ src/data/terms/wdio.ts
@@ -9,6 +9,14 @@ export default {
 
   name: {
     [LOCALES.EN_US]: `WebdriverIO`,
+    [LOCALES.EN_GB]: LOCALES.EN_US,
+    [LOCALES.DE_DE]: LOCALES.EN_US,
+  },
+
+  altName: {
+    [LOCALES.EN_US]: `WDIO`,
+    [LOCALES.EN_GB]: LOCALES.EN_US,
+    [LOCALES.DE_DE]: LOCALES.EN_US,
   },
 
   label: {
