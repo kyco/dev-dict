8a6b84848ad93607eb789feaba1dd2e6d8f03694
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-03-31 17:41:09 +0200

    feat: Add "database" type

---
+++ src/data/types/database.ts
@@ -0,0 +1,12 @@
+import type { TTermType } from '@/types'
+import { LOCALES } from '@/data/locales'
+
+export default {
+  id: `database`,
+
+  name: {
+    [LOCALES.EN_US]: `Database`,
+    [LOCALES.EN_GB]: LOCALES.EN_US,
+    [LOCALES.DE_DE]: `Datenbank`,
+  },
+} as const satisfies TTermType
