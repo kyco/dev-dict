3ce58b280e76f57140da647ffeae60e8cf311979
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-05-29 22:44:39 +0200

    fix: Imports

--- src/data/term-sources.ts
+++ src/data/term-sources.ts
@@ -1,7 +1,7 @@
-import ai_generated from './sources/ai_generated'
-import community from './sources/community'
-import official_website from './sources/official_website'
-import wikipedia from './sources/wikipedia'
+import ai_generated from './term-sources/ai_generated'
+import community from './term-sources/community'
+import official_website from './term-sources/official_website'
+import wikipedia from './term-sources/wikipedia'
 
 export const RAW_SOURCES = {
   [ai_generated.id]: ai_generated,
