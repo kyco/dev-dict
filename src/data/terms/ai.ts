96e12f712d9e8c0672a9587cce23213be163d757
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-05-29 16:04:02 +0200

    fix: Update AI term

--- src/data/terms/ai.ts
+++ src/data/terms/ai.ts
@@ -20,11 +20,15 @@ export default {
   },
 
   label: {
-    [LOCALES.EN_US]: ``,
+    [LOCALES.EN_US]: `AI`,
+    [LOCALES.EN_GB]: LOCALES.EN_US,
+    [LOCALES.DE_DE]: `KI`,
   },
 
   definition: {
-    [LOCALES.EN_US]: ``,
+    [LOCALES.EN_US]: `Artificial Intelligence (AI) refers to the simulation of human intelligence in machines that are programmed to think and learn like humans. It encompasses a wide range of technologies and techniques, including machine learning, natural language processing, computer vision, and robotics. AI systems can perform tasks that typically require human intelligence, such as recognizing speech, making decisions, and solving problems.`,
+    [LOCALES.EN_GB]: LOCALES.EN_US,
+    [LOCALES.DE_DE]: `Künstliche Intelligenz (KI) bezieht sich auf die Simulation menschlicher Intelligenz in Maschinen, die so programmiert sind, dass sie wie Menschen denken und lernen können. Sie umfasst eine Vielzahl von Technologien und Techniken, einschließlich maschinellem Lernen, natürlicher Sprachverarbeitung, Computer Vision und Robotik. KI-Systeme können Aufgaben ausführen, die typischerweise menschliche Intelligenz erfordern, wie z.B. Spracherkennung, Entscheidungsfindung und Problemlösung.`,
   },
 
   type: [],
